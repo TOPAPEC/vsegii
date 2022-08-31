ee4b2003b43b8e7fceabf3507915d5233267b85a
TOPAPEC <danbugrienko@gmail.com>
2022-08-31 21:43:55 +0300

    First commit

---
+++ src/Actors/ActorBase.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+
+export var gravity: = 5000.0
+
+
+func _physics_process(delta: float) -> void:
+	pass
---
+++ src/Actors/Actor_base.gd
@@ -0,0 +1,6 @@
+class ActorBase extends KinematicBody2D:
+	export var gravity: = 5000.0
+
+
+	func _physics_process(delta: float) -> void:
+		pass
